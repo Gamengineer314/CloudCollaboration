044bb924dcb2ded8709b33227aa4a3c598a29c9e
Gamengineer314 <gamengineer314@gmail.com>
2024-08-18 15:07:25 +0200

    Init extension

---
+++ src/extension.ts
@@ -0,0 +1,13 @@
+import * as vscode from 'vscode';
+
+
+// Function called when the extension is activated
+export function activate(context: vscode.ExtensionContext) {
+
+}
+
+
+// Function called when the extension is deactivated
+export function deactivate() {
+
+}
